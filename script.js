a1a51e99e8b5b73b8017adaa2c978023abc8ad12
Z. B. Cirit <zbcirit@gmail.com>
2021-06-26 23:56:51 +0300

    modify style.css script.js and index.html

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const myCanvas = document.querySelector('.canvas');
 const sizeBtn = document.querySelector("#size-btn");
 const randBtn = document.querySelector("#random-btn");
+const titleColor = document.querySelector("#title");
 
 function createCell (number) {
     const itemNumber = number**2;
     for (let i= 0;i<itemNumber;i++){
-        const cell = document.createElement('div');
+        const cell = document.createElement('div')
         cell.style.outline = "1px solid #eee";
         cell.classList.add('cell');
         myCanvas.appendChild(cell);
     }
 }
-myCanvas.style.height = '80vh';
+//set initial canvas to a 16x16 grid
+myCanvas.style.height = '600px';
 createCell(16);
 createGrid(16);
 listen();
 
 function createGrid(columnNumber) {
     myCanvas.style.display = "grid";
-    //container.style.justifyContent = "right";
-    myCanvas.style.gridTemplateColumns = `repeat(${columnNumber},${myCanvas.offsetHeight/columnNumber}px)`;
+    myCanvas.style.gridTemplateColumns = `repeat(${columnNumber},${myCanvas.offsetWidth/columnNumber}px)`;
+
 }
 
 function listen(){
@@ -59,10 +61,18 @@ radioBtns.forEach((btn) => {
 let penColor = "black";
 
 function setPenColor (e) {
+    if(titleColor.classList.contains("gradient-text")) titleColor.classList.remove("gradient-text");
     if (this.value == "random") {
         penColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
-        console.log(penColor);
+        // console.log(penColor);
+        titleColor.style.color=penColor;
     } else if (this.value == "gradient"){
         penColor = "gradient";
-    } else penColor = "black";
+        titleColor.classList.add("gradient-text");
+    } else {
+        penColor = "black";
+        titleColor.style.color=penColor;
+    }
+    
+    console.log(titleColor, penColor);
 }
